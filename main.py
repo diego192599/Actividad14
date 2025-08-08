2c6a6e3ceb491c65a2e82848f249893380c43155
diego192599 <di.cramire@udeo.edu.gt>
2025-08-08 10:12:18 -0600

    creacion de clase participante y metodo str

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Menu")
+class Participantes:
+    def __init__(self,dorsal,nombre,edad,categoria):
+        if not dorsal or nombre or edad or categoria<0:
+            raise ValueError("Datos no validos")
+        self.dorsal=dorsal
+        self.nombre=nombre
+        self.edad=edad
+        self.categoria=categoria
+    def __str__(self):
+        return (f"{self.dorsal}- Nombre: {self.nombre}- Edad: {self.edad} y Categoria: {self.categoria}")
+
+    def agregar(self):
