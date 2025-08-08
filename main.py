26d9dd26bbe8d4bd46c652111cde3ef9f5805f92
diego192599 <di.cramire@udeo.edu.gt>
2025-08-08 10:16:41 -0600

    creacion de metodo de agregar

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+parrticipante={}
 class Participantes:
     def __init__(self,dorsal,nombre,edad,categoria):
         if not dorsal or nombre or edad or categoria<0:
@@ -10,3 +11,17 @@ class Participantes:
         return (f"{self.dorsal}- Nombre: {self.nombre}- Edad: {self.edad} y Categoria: {self.categoria}")
 
     def agregar(self):
+        cantidad=int(input("Ingrese la cantidad de corredores que habran: "))
+        for i in range(cantidad):
+            print(f"Ingrese datos de corredor #{i+1} ")
+            dorsal=input("Ingrese su dorsal: ")
+            nombre=input("Ingrese su nombre completo: ")
+            edad=int(input("Ingrese su edad: "))
+            categoria=input("Ingrese la categoria donde competira: ")
+            parrticipante[nombre]={
+                "dorsal":dorsal,
+                "edad":edad,
+                "categoria":categoria
+            }
+
+    def Ordene
